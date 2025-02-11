9cb98886f01934019f72be45ff55f067f0b6dd41
issac <jins3569@gmail.com>
2025-02-12 00:47:08 +0900

    fix: change ticker css

--- src/_ui/card/CoinTickerCard.tsx
+++ src/_ui/card/CoinTickerCard.tsx
@@ -5,6 +5,7 @@ import Txt from '../typography/Txt';
 
 type Types = {
     market: string;
+    korean_name: string;
     opening_price: number;
     high_price: number;
     low_price: number;
@@ -17,9 +18,11 @@ type Types = {
     acc_trade_price_24h: number;
     acc_trade_volume_24h: number;
 } & HTMLAttributes<HTMLDivElement>;
+
 export const CoinTickerCard = (props: Types) => {
     const {
         market,
+        korean_name,
         opening_price,
         high_price,
         low_price,
@@ -36,30 +39,33 @@ export const CoinTickerCard = (props: Types) => {
 
     return (
         <V.Column {...rest} css={{ border: `1px solid #f5f5f5`, padding: '10px 15px' }}>
-            <V.Row css={{ width: '300px', justifyContent: 'space-between' }}>
-                <V.Column>
-                    <Txt size={18}>{market}</Txt>
-                    <Txt>{change}</Txt>
-                    <Txt>현재가: {trade_price}</Txt>
+            <V.Row css={{ width: '300px', alignItems: 'center', gap: '10px' }}>
+                <V.Column css={{ width: '150px' }}>
+                    <Txt>{korean_name}</Txt>
+                </V.Column>
+                <V.Column css={{ width: '50px', alignItems: 'center' }}>
+                    <Txt>{trade_price}</Txt>
+                </V.Column>
+                <V.Column css={{ width: '100px', alignItems: 'end' }}>
                     <Txt
                         size={20}
                         css={{ color: change === 'RISE' ? colors.red : change === 'FALL' ? colors.blue : colors.text }}
                     >
-                        변동률: {Math.floor(signed_change_rate * 10000) / 100}%
+                        {Math.floor(signed_change_rate * 10000) / 100}%
                     </Txt>
                 </V.Column>
-                <V.Column>
+                {/* <V.Column>
                     <Txt>시가: {opening_price.toPrecision(1)}</Txt>
                     <Txt>고가: {high_price.toPrecision(1)}</Txt>
                     <Txt>저가: {low_price.toPrecision(1)}</Txt>
                     <Txt>종가: {trade_price.toPrecision(1)}</Txt>
                     <Txt>전일 종가: {prev_closing_price.toPrecision(1)}</Txt>
-                </V.Column>
+                </V.Column> */}
             </V.Row>
-            <V.Column>
+            {/* <V.Column>
                 <Txt>누적 거래 대금: {acc_trade_price_24h}</Txt>
                 <Txt>누적 거래량: {acc_trade_volume_24h}</Txt>
-            </V.Column>
+            </V.Column> */}
         </V.Column>
     );
 };
--- src/components/client/home/TickerPanel.tsx
+++ src/components/client/home/TickerPanel.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 
 const TickerPanel = () => {
     const [realtime, setRealtime] = useState(false);
+    const [lowTradeShow, setLowTradeShow] = useState(false);
     const [coins, setCoins] = useState<any[]>([]);
     const [tickers, setTickers] = useState<any[]>([]);
 
@@ -21,9 +22,17 @@ const TickerPanel = () => {
         queryKey: ['ticker'],
         queryFn: async () => {
             const tickers = await getTicker({ markets: coins.map((coin) => coin.market).join(',') });
-            tickers.sort((a: any, b: any) => b.signed_change_rate - a.signed_change_rate);
 
-            setTickers(tickers.filter((el: any) => el.acc_trade_price_24h > 1000000000));
+            const mergedList = Object.values(
+                [...coins, ...tickers].reduce((acc, item) => {
+                    acc[item.market] = { ...acc[item.market], ...item };
+                    return acc;
+                }, {})
+            );
+
+            mergedList.sort((a: any, b: any) => b.signed_change_rate - a.signed_change_rate);
+
+            setTickers(lowTradeShow ? mergedList : mergedList.filter((el: any) => el.acc_trade_price_24h > 1000000000));
         },
         enabled: coins.length > 0 && realtime,
         refetchInterval: 100,
@@ -53,18 +62,25 @@ const TickerPanel = () => {
 
     return (
         <Panel title='현재가 정보' css={{ maxWidth: '600px' }}>
-            <V.Row css={{ gap: '10px', justifyContent: 'space-between' }}>
+            <V.Row css={{ gap: '10px', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Txt>코인 개수: {tickers.length}</Txt>
-                <Button onClick={() => setRealtime((s) => !s)} css={{ width: 'auto' }}>
-                    {realtime ? '실시간 중지' : '실시간 시작'}
-                </Button>
+                <V.Row css={{ gap: '10px' }}>
+                    <Button onClick={() => setLowTradeShow((s) => !s)} css={{ width: 'auto' }}>
+                        {lowTradeShow ? '1000백만 미만 숨기기' : '1000백만 미만 보이기'}
+                    </Button>
+                    <Button onClick={() => setRealtime((s) => !s)} css={{ width: 'auto' }}>
+                        {realtime ? '실시간 중지' : '실시간 시작'}
+                    </Button>
+                </V.Row>
             </V.Row>
             <V.Column css={{ gap: '10px' }}>
                 {tickers.map((el, key) => (
                     <V.Row css={{ alignItems: 'center', gap: '10px' }}>
                         <CoinTickerCard key={key} {...el} />
 
-                        <Button onClick={() => mutation.mutate({ market: el.market })}>전액 매수</Button>
+                        <Button onClick={() => mutation.mutate({ market: el.market })} css={{ width: 'auto' }}>
+                            전액 매수
+                        </Button>
                     </V.Row>
                 ))}
             </V.Column>
