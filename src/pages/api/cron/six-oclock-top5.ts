82e905753719098997c0383796c78faf2aa6de7f
issac <jins3569@gmail.com>
2025-02-20 02:12:43 +0900

    test: cron test code

---
+++ src/pages/api/cron/six-oclock-top5.ts
@@ -0,0 +1,34 @@
+import { getCoins, getTicker } from '@/apis/client/bithumb';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const revalidate = 0;
+
+export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    console.log('Cron job is running...');
+    const markets = (await getCoins({})).filter((el: any) => el.market.split('-').at(0) === 'KRW');
+    const tickers = await getTicker({ markets: (markets || []).map((coin: any) => coin.market).join(',') });
+
+    const mergedList = Object.values(
+        [...markets, ...tickers].reduce((acc, item) => {
+            acc[item.market] = { ...acc[item.market], ...item };
+            return acc;
+        }, {})
+    ).filter((el: any) => el.market !== 'KRW-NFT' && el.market !== 'KRW-BTT');
+
+    const sortedList = mergedList.sort((a: any, b: any) => b.signed_change_rate - a.signed_change_rate);
+
+    console.log(
+        sortedList
+            .map(({ market, korean_name, trade_price, change_rate }: any) => ({
+                market,
+                korean_name,
+                trade_price,
+                change_rate: `${Math.floor(change_rate * 10000) / 100}%`,
+            }))
+            .splice(0, 5)
+    );
+
+    res.status(200).json({ message: 'Task executed' });
+};
+
+export default handler;
--- src/pages/bithumb.tsx
+++ src/pages/bithumb.tsx
@@ -7,7 +7,7 @@ import DetailPanel from '@/components/client/bithumb/DetailPanel';
 import TickerPanel from '@/components/client/bithumb/TickerPanel';
 import { targetTickerStore } from '@/store/bithumb';
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 const Page = () => {
@@ -34,16 +34,53 @@ const Page = () => {
                 }, {})
             ).filter((el: any) => el.market !== 'KRW-NFT' && el.market !== 'KRW-BTT');
 
-            mergedList.sort((a: any, b: any) => b.signed_change_rate - a.signed_change_rate);
+            const sortedList = mergedList.sort((a: any, b: any) => b.signed_change_rate - a.signed_change_rate);
 
             if (!ticker?.market) {
-                setTicker(mergedList.at(0));
+                setTicker(sortedList.at(0));
             }
-            return mergedList;
+            return sortedList;
         },
         enabled: (coins || []).length > 0 && realtime,
         refetchInterval: 200,
     });
+
+    //////////////////////////////////////////
+    // test code
+    useEffect(() => {
+        // 한국 시간 (KST) 오전 6시를 목표로 설정
+        const targetHour = 6;
+        const targetMinute = 1;
+        const targetSecond = 0;
+
+        const checkTimeAndRunTask = () => {
+            const currentTime = new Date();
+
+            // 한국 시간(KST) 계산
+            const kstOffset = 9 * 60; // 한국은 UTC +9
+            const kstTime = new Date(currentTime.getTime());
+
+            const hours = kstTime.getHours();
+            const minutes = kstTime.getMinutes();
+            const seconds = kstTime.getSeconds();
+
+            if (hours === targetHour && minutes === targetMinute && seconds === targetSecond) {
+                console.log('한국시간 오전 6시가 되었습니다. 작업을 실행합니다.');
+                runTask();
+            }
+        };
+
+        const runTask = () => {
+            fetch('/api/cron/six-oclock-top5');
+        };
+
+        // 1초마다 현재 시간을 확인
+        const intervalId = setInterval(checkTimeAndRunTask, 1000);
+
+        // 컴포넌트가 unmount되면 interval을 정리
+        return () => clearInterval(intervalId);
+    }, []);
+    //////////////////////////////////////////
 
     return (
         <View>
